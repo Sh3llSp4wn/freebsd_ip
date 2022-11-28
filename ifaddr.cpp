d8b0bef56e5fe02752f326853763b00e5a55b12d
shellspawn <shellspawn@paimon.lan>
2022-11-28 00:14:15 -0600

    added try catch for internal runtime_error use

--- ifaddr.cpp
+++ ifaddr.cpp
@@ -50,42 +50,45 @@ int main(int ac, char* av[]){
   // create the interface object pointer
   iface_obj obj;
 
+  try {
+    if(!collect_arguments(ac, av, vm)) return 1;
 
-  if(!collect_arguments(ac, av, vm)) return 1;
-
-  if(vm.count("object")){
-    // use addr object, make_unique 
-    if(is_str(vm, "object", "addr")
-        || is_str(vm, "object", "a"))
-      obj = std::make_unique<addr_obj>();
-    // use route object
-    else if(is_str(vm, "object", "route")
-        || is_str(vm, "object", "r"))
-      obj = std::make_unique<route_obj>();
-    // use tun object
-    else if(is_str(vm, "object", "tun")
-        || is_str(vm, "object", "t"))
-      obj = std::make_unique<tun_obj>();
-    else {
-      std::cerr << "invalid object selection" << std::endl;
-      return 1;
+    if(vm.count("object")){
+      // use addr object, make_unique 
+      if(is_str(vm, "object", "addr")
+          || is_str(vm, "object", "a"))
+        obj = std::make_unique<addr_obj>();
+      // use route object
+      else if(is_str(vm, "object", "route")
+          || is_str(vm, "object", "r"))
+        obj = std::make_unique<route_obj>();
+      // use tun object
+      else if(is_str(vm, "object", "tun")
+          || is_str(vm, "object", "t"))
+        obj = std::make_unique<tun_obj>();
+      else {
+        std::cerr << "invalid object selection" << std::endl;
+        return 1;
+      }
     }
-  }
 
-  // determine which function to call
-  if(vm.count("action")){
-    if(is_str(vm, "action", "list") 
-      || is_str(vm, "action", "l"))
-        obj->list(vm);
-    else if(is_str(vm, "action", "add")
-      || is_str(vm, "action", "a"))
-        obj->add(vm);
-    else if(is_str(vm, "action", "del")
-      || is_str(vm, "action", "d"))
-        obj->del(vm);
-    else{
-      std::cerr << "invalid action selection" << std::endl;
-      return 1;
+    // determine which function to call
+    if(vm.count("action")){
+      if(is_str(vm, "action", "list") 
+        || is_str(vm, "action", "l"))
+          obj->list(vm);
+      else if(is_str(vm, "action", "add")
+        || is_str(vm, "action", "a"))
+          obj->add(vm);
+      else if(is_str(vm, "action", "del")
+        || is_str(vm, "action", "d"))
+          obj->del(vm);
+      else{
+        std::cerr << "invalid action selection" << std::endl;
+        return 1;
+      }
     }
+  } catch (std::runtime_error& ex){
+    std::cerr << "ERROR: " << ex.what() << std::endl;
   }
 }
